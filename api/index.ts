e69857d0ee4a502b742c0e684a4c5a55111658dc
catslovefish.eth <1036302150@qq.com>
2024-11-17 08:13:50 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,17 @@
 import { parseGwei, stringToHex, toBlobs } from 'viem';
-import { client } from './client';
+import { createWalletClient, http,  } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { mainnet, sepolia } from "viem/chains";
+
+ const account = privateKeyToAccount("0x4b1012dbed8a68b9fea640437d2d01a22d9b04ecedacb7188baecb6da1d66b4d");
+
+const client = createWalletClient({
+  account,
+  chain: sepolia,
+//   transport: http("https://sepolia.blockpi.network/v1/rpc/26bf80a1194ddc50a6f2fbdd851808c751b5b02e"),
+  transport: http(),
+});
+
 import { kzg } from './kzg';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
